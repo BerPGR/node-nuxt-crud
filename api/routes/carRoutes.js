8179060523311c8b5df4998896f05b102f65234d
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-03-06 17:09:06 -0300

    Change page design in layout and added cars page

--- api/routes/carRoutes.js
+++ api/routes/carRoutes.js
@@ -1,10 +1,32 @@
 const router = require('express').Router()
 const Car = require('../models/Car')
 
-router.get('/', (req, res) => {
-    
+router.get('/', async (req, res) => {
+    const cars = await Car.find({})
+
+    if(!cars) {
+        res.send({message: 'There are no cars in the database'})
+        return
+    }
+
+    res.send(cars)
 })
 
+router.post('/', async (req, res) => {
+    const { marca, modelo, ano } = req.body
+
+    const car = {
+        marca, modelo, ano
+    }
+
+    try {
+        await Car.create(car)
+        console.log('Car criado')
+    }
+    catch(e) {
+        console.log(e)
+    }
+})
 
 
 module.exports = router;
---
+++ pages/cars.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-row align="center" justify="center">
+        <v-col cols="12">
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
